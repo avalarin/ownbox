5823b0203574b6b857cfdbea0a784304077980be
avalarin <avalarin@gmail.com>
2014-05-01 21:26:05 +0700

    Добавлен контроллер и представления для регистрации

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,37 @@
+class RegistrationController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    params.require(:user).permit! #(:name, :display_name, :email, :password, :password_confirmation, :commit)
+    @user = User.new params[:user]
+    @user.activation_code = SecureRandom.uuid
+    if @user.valid?
+      @user.save!
+      UserMailer.success_registration(@user).deliver
+      redirect_to action: 'success'
+    else
+      render 'new'
+    end
+  end
+
+  def success
+
+  end
+
+  def activate
+    if params[:code]
+      user = User.find_by_activation_code params[:code]
+      if user
+        user.approved = true
+        user.activation_code = nil
+        user.save!
+        flash[:notice] = t '.account_activated'
+      end
+    end
+    redirect_to login_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ FilesBrowser::Application.routes.draw do
   post '/login', to: 'session#create', as: :login_post
   get '/logout', to: 'session#destroy', as: :logout
 
+  get '/register', to: 'registration#new', as: :register_user
+  post '/register', to: 'registration#create', as: :create_user
+  get '/register/success', to: 'registration#success', as: :register_user_success
+  get '/register/activate', to: 'registration#activate', as: :activate_user
+
   get '/get(/:user_name(/*path))', to: 'file#get', as: :get_item, :format => false
   get '/preview(/:user_name(/*path))', to: 'file#preview', as: :preview_item, :format => false
   get '/home(/*path)', to: 'directory#index', defaults: { user_name: :current_user }, as: :current_user_items_index, :format => false
