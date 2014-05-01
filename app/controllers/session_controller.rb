60a5c0ac57b37499c362668d2d9fe833f3ff587f
avalarin <avalarin@gmail.com>
2014-05-01 21:17:39 +0700

    Исправлен баг изза которого войти на сайт можно было с любым паролем

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -14,12 +14,15 @@ class SessionController < ApplicationController
     if (user)
         if (user.locked)
             flash.now.alert = t('errors.messages.user_locked')
+        elsif (!user.approved)
+            flash.now.alert = t('errors.messages.account_not_activated')
         elsif (!user.authenticate(login[:password]))
             flash.now.alert = t('errors.messages.invalid_email_or_password')
+        else
+          login_user user, login[:remember]
+          redirect_to root_path
+          return
         end
-        login_user user, login[:remember]
-        redirect_to root_path
-        return
     else 
         flash.now.alert = t('errors.messages.invalid_email_or_password')
     end
