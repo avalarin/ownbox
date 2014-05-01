1c4c1a8f6d533dd12e85bc759b275ec69e942c52
avalarin <avalarin@gmail.com>
2014-05-01 21:20:12 +0700

    Поле password является обязательным только при создании пользователя, home_directory не обязательно

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
 class User < ActiveRecord::Base
   self.table_name = 'security.users'
-  before_save { self.email = email.downcase }
+  before_save { 
+    self.email = email.downcase 
+    self.home_directory ||= name 
+  }
   
-  validates :name, :email, :home_directory, :display_name, presence: true
+  validates :name, :email, :display_name, presence: true
   validates :name, :email, uniqueness: { case_sensitive: false }
   validates :name, length: { minimum: 5 }
   validates :email, email_format: { message: "doesn't look like an email address" }
 
   has_secure_password
-  validates :password, length: { minimum: 5 }
+  validates :password, :presence =>true, :confirmation => true, :length => { :within => 5..40 }, :on => :create
+  validates :password, :confirmation => true, :length => { :within => 5..40 }, :on => :update, :unless => lambda{ |user| user.password.blank? }
 
   def full_home_directory
     base = File.expand_path(Settings['home_directories_path'])
