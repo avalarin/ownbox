d0386c4a96b4961fd2d933b8c511da8e891130f3
avalarin <avalarin@gmail.com>
2014-06-19 00:57:13 +0700

    Строковые ключи заменены на символьные

--- config/initializers/01_settings.rb
+++ config/initializers/01_settings.rb
@@ -3,21 +3,23 @@ class Settings < Settingslogic
     namespace Rails.env
 
     # Настройки по умолчанию
-    Settings['app_name'] ||= 'Ownbox'
+    Settings[:app_name] ||= 'Ownbox'
 
     # Папка для хранения пользовательских данных
-    Settings['home_directories_path'] ||= 'data'
-    unless File.directory?(Settings['home_directories_path'])
-      raise 'Directory ' + File.expand_path(Settings['home_directories_path']) + ' not found'
+    Settings[:home_directories_path] ||= 'data'
+    unless File.directory?(Settings[:home_directories_path])
+      raise 'Directory ' + File.expand_path(Settings[:home_directories_path]) + ' not found'
     end
     
     # Настройки безопасности   
-    Settings['security'] ||= Settingslogic.new({})
-    Settings.security['session_lifetime'] ||= 1.hours
-    Settings.security['persistent_session_lifetime'] ||= 30.days
+    Settings[:security] ||= Settingslogic.new({})
+    Settings.security[:session_lifetime] ||= 1.hours
+    Settings.security[:persistent_session_lifetime] ||= 30.days
 
     # Настройки отправки почты
-    Settings['mailer'] ||= Settingslogic.new({})
-    Settings.mailer['from'] ||= 'ownbox@example.com'
+    Settings[:mailer] ||= Settingslogic.new({})
+    Settings.mailer[:from] ||= 'ownbox@avalarin.net'
 
+    Settings[:admin] ||= Settingslogic.new({}) 
+    Settings.admin[:page_size] ||= 12
 end
